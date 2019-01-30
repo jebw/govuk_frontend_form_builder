0f26439323d223fcb4ecab9f93b0dbac82c2d124
jebw <jeb@jdwilkins.co.uk>
2019-01-30 15:20:21 +0000

    Renamed files for new name

--- lib/govuk_elements_form_builder.rb
+++ lib/govuk_elements_form_builder.rb
@@ -1,7 +1,7 @@
-require 'govuk_elements_form_builder/version'
-require 'govuk_elements_form_builder/form_builder'
-require 'govuk_elements_form_builder/block_buffer'
-require_relative '../app/helpers/govuk_elements_errors_helper'
+require 'govuk_frontend_form_builder/version'
+require 'govuk_frontend_form_builder/form_builder'
+require 'govuk_frontend_form_builder/block_buffer'
+require_relative '../app/helpers/govuk_frontend_errors_helper'
 
-module GovukElementsFormBuilder
+module GovukFrontendFormBuilder
 end
